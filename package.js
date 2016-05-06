5fc78445a5327947a18f1ae96a86de04d81e2d1c
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2016-05-06 16:14:44 +0300

    add git url

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'osv:paginate-next-prev',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Pagination with previous and next navigation.',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/osv/meteor-paginate-next-prev.git',
   documentation: 'README.md'
 });
 
